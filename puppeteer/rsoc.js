7fe81036c43bcb795cfa6b4bb1a18e7414a47673
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-04-04 15:29:58 +0100

    Add initial Puppeteer scraper.

---
+++ puppeteer/rsoc.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+
+const browser = await puppeteer.launch();
+
+const page = await browser.newPage();
+await page.goto('https://royalsocietypublishing.org/action/doSearch?text1=respirator&openAccess=18');
+
+//console.log(await page.content());
+
+var repeat = true;
+while (repeat) {
+    await page.waitFor('nav[class="pagination"]');
+    const pdfs = await page.$$eval('a[title="PDF"]', as => as.map(a => a.href));
+    for (i in pdfs) {
+      console.log(pdfs[i]);
+    }
+    np = await page.$('a[title="Next Page"]');
+    if (np === null ) {
+      repeat = false;
+    } else {
+      await page.click('a[title="Next Page"]');
+    }
+}
+
+await page.screenshot({path: 'screenshot.png'});
+
+})();
+
---
+++ puppeteer/run-with-docker.sh
@@ -0,0 +1,1 @@
+docker run --shm-size 1G --rm -v /home/anj/openVirus/puppeteer/rsoc.js:/app/index.js alekzonder/puppeteer:latest
